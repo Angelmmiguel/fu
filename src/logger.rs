6c54dcabc98899b21f4248ef7acd6f866742d501
Ángel M <Angelmmiguel@users.noreply.github.com>
2022-02-25 19:34:37 +0100

    [#3] feat: add colors to the output and the --no-colors option to disable it (#15)

--- src/logger.rs
+++ src/logger.rs
@@ -1,4 +1,5 @@
 use crate::FileSize;
+use colored::*;
 
 const KBYTES:u64 = 1024;
 const MBYTES:u64 = 1048576;
@@ -8,9 +9,15 @@ pub fn print_headers() {
   println!("DISK\tBYTES\tPATH");
 }
 
-pub fn print_size(results: &Vec<FileSize>) {
+pub fn print_size(results: &Vec<FileSize>, no_colors: bool) {
   for result in results {
-    println!("{}\t{}\t{}", human_size(result.disk), human_size(result.bytes), result.path);
+    let output = format!("{}\t{}\t{}", human_size(result.disk), human_size(result.bytes), result.path);
+
+    if no_colors {
+      println!("{}", output);
+    } else {
+      println!("{}", colorize_size(output, result.bytes));
+    }
   }
 }
 
@@ -24,4 +31,22 @@ fn human_size(size: u64) -> String {
   } else {
     return format!("{}B", size);
   }
+}
+
+fn colorize_size(str: String, size: u64) -> String {
+  let colored_string: ColoredString;
+
+  if size > GBYTES {
+    colored_string = str.red();
+  } else if size > 512 * MBYTES {
+    colored_string = str.yellow();
+  } else if size > 128 * MBYTES {
+    colored_string = str.magenta();
+  } else if size > MBYTES {
+    colored_string = str.cyan();
+  } else {
+    colored_string = str.green();
+  }
+
+  return colored_string.to_string();
 }
