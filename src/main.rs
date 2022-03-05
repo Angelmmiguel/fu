528aa962329a5618e2b3a6752c62115bdf5210a6
Ángel M <Angelmmiguel@users.noreply.github.com>
2022-03-05 23:04:58 +0100

    [#34] feat: add editorconfig and fix indentation (#35)

--- src/main.rs
+++ src/main.rs
@@ -11,107 +11,107 @@ mod logger;
 mod size;
 
 use crate::size::{
-    entry_size,
-    FileSize
+  entry_size,
+  FileSize
 };
 use crate::logger::{
-    print_size,
-    print_headers
+  print_size,
+  print_headers
 };
 
 /// Retrieve the size and disk usage of files and directories
 #[derive(Parser)]
 #[clap(version = env!("FU_VERSION"))]
 struct Cli {
-    /// The path to the file to read
-    #[clap(parse(from_os_str))]
-    path: std::path::PathBuf,
+  /// The path to the file to read
+  #[clap(parse(from_os_str))]
+  path: std::path::PathBuf,
 
-    /// Sort the output based on the size
-    #[clap(long, short)]
-    sort: bool,
+  /// Sort the output based on the size
+  #[clap(long, short)]
+  sort: bool,
 
-    /// Hide the headers from the output
-    #[clap(long)]
-    no_header: bool,
+  /// Hide the headers from the output
+  #[clap(long)]
+  no_header: bool,
 
-    /// Disable the colors in the output
-    #[clap(long)]
-    no_colors: bool,
+  /// Disable the colors in the output
+  #[clap(long)]
+  no_colors: bool,
 
-    /// Sort and limit the output to the N heaviest entries
-    #[clap(long, short, parse(try_from_str))]
-    top: Option<usize>
+  /// Sort and limit the output to the N heaviest entries
+  #[clap(long, short, parse(try_from_str))]
+  top: Option<usize>
 }
 
 fn main() -> std::io::Result<()> {
-    let args = Cli::parse();
-    let mut results: Vec<FileSize> = vec![];
+  let args = Cli::parse();
+  let mut results: Vec<FileSize> = vec![];
 
-    // Decide which logic to follow
-    let args_str = args.path.to_string_lossy();
+  // Decide which logic to follow
+  let args_str = args.path.to_string_lossy();
 
-    if args_str.contains("*") {
-        // Glob
-        for entry in glob(&args_str).expect("The glob pattern is invalid") {
-            match entry {
-                Ok(path) => {
-                    match entry_size(&path) {
-                        Ok(size) => results.push(size),
-                        Err(e) => {
-                            eprintln!("{}", e.to_string().red());
-                            exit(1);
-                        }
-                    }
-                },
-                Err(e) => println!("{:?}", e),
-            }
-        }
-    } else if args_str.ends_with("/") {
-        // Use glob too
-        let args_with_glob = format!("{}*", args_str);
-        // List directory
-        for entry in glob(&args_with_glob).expect("The glob pattern is invalid") {
-            match entry {
-                Ok(path) => {
-                    match entry_size(&path) {
-                        Ok(size) => results.push(size),
-                        Err(e) => {
-                            eprintln!("{}", e.to_string().red());
-                            exit(1);
-                        }
-                    }
-                },
-                Err(e) => println!("{:?}", e),
-            }
-        }
-    } else {
-        // Single file
-        match entry_size(&args.path) {
+  if args_str.contains("*") {
+    // Glob
+    for entry in glob(&args_str).expect("The glob pattern is invalid") {
+      match entry {
+        Ok(path) => {
+          match entry_size(&path) {
             Ok(size) => results.push(size),
             Err(e) => {
-                eprintln!("{}", e.to_string().red());
-                exit(1);
+              eprintln!("{}", e.to_string().red());
+              exit(1);
             }
-        }
-    }
-
-    if !args.no_header {
-        print_headers()
-    }
-
-    if args.sort || args.top.is_some() {
-        results.sort_by(|a, b| b.bytes.cmp(&a.bytes));
-    }
-
-    match args.top {
-        Some(num) => {
-            results.truncate(num);
+          }
         },
-        None => {}
+        Err(e) => println!("{:?}", e),
+      }
     }
+  } else if args_str.ends_with("/") {
+    // Use glob too
+    let args_with_glob = format!("{}*", args_str);
+    // List directory
+    for entry in glob(&args_with_glob).expect("The glob pattern is invalid") {
+      match entry {
+        Ok(path) => {
+          match entry_size(&path) {
+            Ok(size) => results.push(size),
+            Err(e) => {
+              eprintln!("{}", e.to_string().red());
+              exit(1);
+            }
+          }
+        },
+        Err(e) => println!("{:?}", e),
+      }
+    }
+  } else {
+    // Single file
+    match entry_size(&args.path) {
+      Ok(size) => results.push(size),
+      Err(e) => {
+        eprintln!("{}", e.to_string().red());
+        exit(1);
+      }
+    }
+  }
 
-    print_size(&results, args.no_colors);
+  if !args.no_header {
+    print_headers()
+  }
 
-    Ok(())
+  if args.sort || args.top.is_some() {
+    results.sort_by(|a, b| b.bytes.cmp(&a.bytes));
+  }
+
+  match args.top {
+    Some(num) => {
+      results.truncate(num);
+    },
+    None => {}
+  }
+
+  print_size(&results, args.no_colors);
+
+  Ok(())
 }
--- src/size.rs
+++ src/size.rs
@@ -17,28 +17,28 @@ pub fn entry_size(path: &std::path::PathBuf) -> std::io::Result<FileSize> {
   let mut disk_acc = 0;
 
   if metadata_entry.is_dir() {
-      for entry in fs::read_dir(path)? {
-          let entry = entry?;
-          let inner_path = entry.path();
-          if inner_path.is_dir() {
-              let inner_entry_size = entry_size(&inner_path)?;
-              disk_acc = disk_acc + inner_entry_size.disk;
-              acc = acc + inner_entry_size.bytes;
-          } else {
-              disk_acc = disk_acc + inner_path.size_on_disk()?;
-              let metadata = fs::metadata(&inner_path)?;
-              acc = acc + metadata.len();
-          }
+    for entry in fs::read_dir(path)? {
+      let entry = entry?;
+      let inner_path = entry.path();
+      if inner_path.is_dir() {
+        let inner_entry_size = entry_size(&inner_path)?;
+        disk_acc = disk_acc + inner_entry_size.disk;
+        acc = acc + inner_entry_size.bytes;
+      } else {
+        disk_acc = disk_acc + inner_path.size_on_disk()?;
+        let metadata = fs::metadata(&inner_path)?;
+        acc = acc + metadata.len();
       }
+    }
   } else {
-      disk_acc = disk_acc + path.size_on_disk()?;
-      acc = acc + metadata_entry.len();
+    disk_acc = disk_acc + path.size_on_disk()?;
+    acc = acc + metadata_entry.len();
   }
 
   let res = FileSize {
-      bytes: acc,
-      disk: disk_acc,
-      path: path.display().to_string()
+    bytes: acc,
+    disk: disk_acc,
+    path: path.display().to_string()
   };
 
   Ok(res)
