304b7a80a58438b35ccb8c00634b05fcd0c70063
Ángel M <hi@angel.kiwi>
2023-02-03 21:44:27 +0100

    feat: add clippy and fmt. Run them on the CI (#45)

--- build.rs
+++ build.rs
@@ -3,15 +3,14 @@ use std::process::Command;
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() -> std::io::Result<()> {
-  // Obtain the current Git commit SHA
-  let output = 
-    Command::new("git")
-      .args(&["rev-parse", "HEAD"])
-      .output()
-      .unwrap();
-  let git_hash = String::from_utf8(output.stdout).unwrap();
+    // Obtain the current Git commit SHA
+    let output = Command::new("git")
+        .args(["rev-parse", "HEAD"])
+        .output()
+        .unwrap();
+    let git_hash = String::from_utf8(output.stdout).unwrap();
 
-  println!("cargo:rustc-env=FU_VERSION={} - Git commit: {}", VERSION, git_hash);
+    println!("cargo:rustc-env=FU_VERSION={VERSION} - Git commit: {git_hash}");
 
-  Ok(())
+    Ok(())
 }
